fb2837fa7687c75e2dada19d7ba392cbcb3b5329
Alexander Greco <33609792+Alexander5015@users.noreply.github.com>
2020-07-15 13:32:32 -0600

    Convert Intake and Shooter to subsystems

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,18 +7,130 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.cuforge.libcu.Lasershark;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase; 
+
+/**
+ * Ball intake code
+ *
+ * @author Alexander Greco
+ */
+
+
+/**
+ * The intake class controls the ball intake and storage.
+ * It will automatically intake, but not too many balls.
+ */
 public class IntakeSubsystem extends SubsystemBase {
-    /**
-     * Creates a new ExampleSubsystem.
-     */
+
+    // private final double beltCircumference = 0.0 * Math.PI;
+    // private final double magazineTicksPerBall = 0.0 / beltCircumference * 7.5;
+
+    private double magazineSpeed = -0.70;
+    private double intakeSpeed = 1.0;
+    private VictorSPX magazineMotor = new VictorSPX(6);
+    private VictorSPX intakeMotor = new VictorSPX(5);
+    private Lasershark intakeDetector = new Lasershark(5);
+    private DoubleSolenoid intakeControl = new DoubleSolenoid(6, 7);
+
+    private double ballsStored = 3;
+    private boolean safetyOverride = false;
+
+
     public IntakeSubsystem() {
+        SmartDashboard.putBoolean("Override", safetyOverride);
+        SmartDashboard.putNumber("Balls Stored", ballsStored);
+    }
 
+
+    public void setMagazine(boolean running) {
+        setMagazine(running, magazineSpeed);
+    }
+
+    public void setMagazine(boolean running, double speed) {
+        if (running) {
+            magazineMotor.set(ControlMode.PercentOutput, speed);
+        } else {
+            magazineMotor.set(ControlMode.PercentOutput, 0.0);
+        }
+    }
+
+    public void reverseMagazine(boolean reverse){
+        if(reverse) {
+            magazineSpeed = Math.abs(magazineSpeed);
+        }
+        else {
+            magazineSpeed = -Math.abs(magazineSpeed);
+        }
+    }
+
+    public void reverseMagazine(){
+        magazineSpeed = -magazineSpeed;
+    }
+
+  /**
+   * Sets the amount of balls stored for a user-override.
+   */
+  public void setBallsStored(int ballsStored) {
+    this.ballsStored = ballsStored;
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public double getBallsStored() {
+    return ballsStored;
   }
+
+  public void ballShot() {
+    if (ballsStored >= 1) {
+        ballsStored--;
+    }
+    if (ballsStored < 0) {
+        ballsStored = 0;
+    }
+}
+
+  /**
+   * Main update loop for intaking balls automatically.
+   */
+    public boolean getIntakeDectector() {
+
+        if (intakeDetector.getDistanceInches() < 5.0 && intakeDetector.getDistanceInches() != 0.0) {
+
+            return true;
+
+        } else if (intakeDetector.getDistanceInches() == 0.0) {
+
+            DriverStation.reportError("Lasershark Disconnected", false);
+            return false;
+
+        } else {
+            return false;
+        }
+    }
+   
+    //Allow code to control intake motor and solenoid
+    public void setIntake(boolean running) {
+        if(running) {
+            intakeMotor.set(ControlMode.PercentOutput, intakeSpeed);
+            intakeControl.set(Value.kForward);
+        } else {
+            intakeMotor.set(ControlMode.PercentOutput, 0.0);
+            intakeControl.set(Value.kReverse);
+        }
+    }
+
+    public void setIntakeSpeed(double speed){
+        intakeSpeed = speed;
+    }
+    
+    public void reverseIntake(){
+        setIntakeSpeed(-intakeSpeed);
+    }
+    
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,24 +1,157 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.cuforge.libcu.Lasershark;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ControlType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
+/**
+ * Ball shooter code
+ *
+ * @author Alexander Greco
+ */
+
+/**
+ * The shooter class controls the ball intake, storage, and shooter. It will
+ * automatically intake, but not too many balls.
+ */
 public class ShooterSubsystem extends SubsystemBase {
-    /**
-     * Creates a new ExampleSubsystem.
-     */
+
+    // private final double beltCircumference = 0.0 * Math.PI;
+    // private final double magazineTicksPerBall = 0.0 / beltCircumference * 7.5;
+
+    private double shooterSpeed = ShooterConstants.kDefaultShooterSpeed;
+    private double manualShooterSpeed = ShooterConstants.kDefaultShooterSpeed;
+    private CANSparkMax shooterMotor = new CANSparkMax(ShooterConstants.kShooterMotorPort, MotorType.kBrushless);
+    private CANEncoder shooterEncoder = new CANEncoder(shooterMotor);
+    private CANPIDController shooterController = shooterMotor.getPIDController();
+    private Lasershark shootDetector = new Lasershark(ShooterConstants.kLaserSharkPort);
+
+    private double setPoint = 0;
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
+
     public ShooterSubsystem() {
 
-  }
+        // set PID coefficients
+        kP = ShooterConstants.kP;
+        kI = ShooterConstants.kI;
+        kD = ShooterConstants.kD;
+        kIz = ShooterConstants.kIz;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+        // Max rpm
+        kFF = ShooterConstants.kFF;
+
+        kMaxOutput = ShooterConstants.kMaxOutput;
+        kMinOutput = ShooterConstants.kMinOutput;
+        maxRPM = ShooterConstants.kMaxRPM;
+        shooterController.setP(kP);
+        shooterController.setI(kI);
+        shooterController.setD(kD);
+        shooterController.setIZone(kIz);
+        shooterController.setFF(kFF);
+        shooterController.setOutputRange(kMinOutput, kMaxOutput);
+
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("Shooting I Gain", kI);
+        SmartDashboard.putNumber("Shooting D Gain", kD);
+        SmartDashboard.putNumber("Shooting P Gain", kP);
+        SmartDashboard.putNumber("Shooting Feed Forward", kFF);
+        SmartDashboard.putNumber("Shooting Manual Shooter Speed", manualShooterSpeed);
+
+    }
+
+    /**
+     * Sets the intake on or off, uses intakeSpeed for the power if on.
+     * 
+     * @param running Whether the intake wheels should be spinning
+     */
+
+    @Override
+    public void periodic() {
+        
+        // read PID coefficients from SmartDashboard
+        double p = SmartDashboard.getNumber("Shooting P Gain", 0);
+        double i = SmartDashboard.getNumber("Shooting I Gain", 0);
+        double d = SmartDashboard.getNumber("Shooting D Gain", 0);
+        double ff = SmartDashboard.getNumber("Shooting Feed Forward", 0);
+        manualShooterSpeed = SmartDashboard.getNumber("Shooting Manual Shooter Speed", 0.85);
+        // if PID coefficients on SmartDashboard have changed, write new values to
+        // controller
+        if ((i != kI)) {
+            shooterController.setI(i);
+            kI = i;
+        }
+        if ((d != kD)) {
+            shooterController.setD(d);
+            kD = d;
+        }
+        if ((p != kP)) {
+            shooterController.setP(p);
+            kP = p;
+        }
+        if ((ff != kFF)) {
+            shooterController.setFF(ff);
+            kFF = ff;
+        }
+
+        SmartDashboard.putNumber("SetPoint", setPoint);
+        SmartDashboard.putNumber("ProcessVariable", shooterEncoder.getVelocity());
+    }
+
+    public void setShooter(boolean running) {
+        if (running) {
+            shooterController.setOutputRange(kMinOutput, kMaxOutput);
+            setPoint = shooterSpeed * maxRPM;
+        } else {
+            setPoint = 0 * maxRPM;
+            shooterController.setOutputRange(0, 0);
+        }
+        shooterController.setReference(setPoint, ControlType.kVelocity);
+    }
+
+    public void setShooterSpeed(double distance) {
+        double speed = 0.0;
+        // If distance is 0.0 (manual entry), sets speed to 0.85
+        if (distance != 0.0) {
+            distance += 17;
+            // Sets speed based on distance from wall
+            if (distance < 52) {
+                speed = -1.32 + 0.112 * distance + -0.00144 * distance * distance;
+            } else {
+                speed = 0.658 + -0.00244 * distance + 0.0000161 * distance * distance;
+            }
+        } else {
+
+            speed = manualShooterSpeed;
+        }
+
+        shooterSpeed = speed;
+    }
+
+    public double getTemperature() {
+        return shooterMotor.getMotorTemperature();
+    }
+
+	public double getVelocity() {
+		return shooterEncoder.getVelocity();
+    }
+    
+    public double getSetpoint() {
+		return setPoint;
+	}
+
+	public boolean getVelocitySetpoint() {
+		return getVelocity() >= getSetpoint();
+	}
+
+	public boolean ballDetected() {
+		return shootDetector.getDistanceInches() < ShooterConstants.kBallDetectionDistance;
+	}
+
 }
