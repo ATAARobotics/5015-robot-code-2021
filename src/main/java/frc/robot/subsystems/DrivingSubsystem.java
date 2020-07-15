09af7070722f65fcf069fb0a6ab28f6ed6ca376f
BEN1JEN <benpheard@gmail.com>
2020-07-14 19:17:07 -0600

    Add base Driving Subsystem class

---
+++ src/main/java/frc/robot/subsystems/DrivingSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivingSubsystem extends SubsystemBase {
+  /**
+   * Creates a new DrivingSubsystem.
+   */
+  public DrivingSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
