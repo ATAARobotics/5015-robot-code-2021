065ddfb526362c6167751b1823a008bac67adda1
Jacob <Jacob-Guglielmin>
2020-08-11 19:31:24 -0600

    Removed FIRST copyright notice from heavily modified files, moved buttons to constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
@@ -26,6 +19,16 @@ import frc.robot.commands.ShootCommand;
  * mapping should happen here.
  */
 public class RobotContainer {
+   private static final class OIConstants {
+      public static final int kManualShootButton = Button.kY.value;
+      public static final int kDistShootButton = Button.kA.value;
+      public static final int kVisionShootButton = Button.kB.value;
+      public static final int kResetBallsButton = Button.kStart.value;
+      public static final int kMagazineReverseButton = Button.kBack.value;
+      public static final int kIntakeReverseButton = Button.kX.value;
+      public static final int kIntakeButton = Button.kA.value;
+   }
+
    // The robot's subsystems and commands are defined here
    private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
    private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
@@ -45,37 +48,40 @@ public class RobotContainer {
     */
    private void configureButtonBindings() {
       // Manual Shoot
-      new JoystickButton(m_gunnerController, Button.kY.value)
-            .whileHeld(new SequentialCommandGroup(new InstantCommand(() -> m_shooterSubsystem.setShooterSpeed(0.0)),
-                  new ShootCommand(m_shooterSubsystem, m_intakeSubsystem)));
+      new JoystickButton(m_gunnerController, OIConstants.kManualShootButton)
+         .whileHeld(new SequentialCommandGroup(new InstantCommand(() -> m_shooterSubsystem.setShooterSpeed(0.0)),
+            new ShootCommand(m_shooterSubsystem, m_intakeSubsystem)));
       // Dist Shoot
-      new JoystickButton(m_gunnerController, Button.kA.value).whileHeld(new ParallelCommandGroup(
+      new JoystickButton(m_gunnerController, OIConstants.kDistShootButton)
+         .whileHeld(new ParallelCommandGroup(
             new InstantCommand(() -> m_shooterSubsystem.setShooterSpeed(/* TODO Distance input */0)),
             new ShootCommand(m_shooterSubsystem, m_intakeSubsystem)));
 
       // Vision Shoot
-      new JoystickButton(m_gunnerController, Button.kB.value).whenHeld(new ParallelCommandGroup(
+      new JoystickButton(m_gunnerController, OIConstants.kVisionShootButton)
+         .whenHeld(new ParallelCommandGroup(
             /* new VisionTargetingCommand(m_visionSubsystem), */
             new InstantCommand(() -> m_shooterSubsystem.setShooterSpeed(/* S */0)),
             new ShootCommand(m_shooterSubsystem, m_intakeSubsystem)));
 
       // Reset Balls
-      new JoystickButton(m_gunnerController, Button.kStart.value)
-            .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setBallsStored(0)));
+      new JoystickButton(m_gunnerController, OIConstants.kResetBallsButton)
+         .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setBallsStored(0)));
 
       // Magazine Reverse
-      new JoystickButton(m_gunnerController, Button.kBack.value)
-            .whenPressed(new InstantCommand(() -> m_intakeSubsystem.setMagazineReverse()))
-            .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setMagazineOff()));
+      new JoystickButton(m_gunnerController, OIConstants.kMagazineReverseButton)
+         .whenPressed(new InstantCommand(() -> m_intakeSubsystem.setMagazineReverse()))
+         .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setMagazineOff()));
 
       // Intake Reverse
-      new JoystickButton(m_gunnerController, Button.kX.value)
-            .whenPressed(new SequentialCommandGroup(new InstantCommand(() -> m_intakeSubsystem.setMagazineReverse()),
-                  new InstantCommand(() -> m_intakeSubsystem.setIntakeReverse())))
-            .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setMagazineOff()));
+      new JoystickButton(m_gunnerController, OIConstants.kIntakeReverseButton)
+         .whenPressed(new SequentialCommandGroup(
+            new InstantCommand(() -> m_intakeSubsystem.setMagazineReverse()),
+            new InstantCommand(() -> m_intakeSubsystem.setIntakeReverse())))
+         .whenReleased(new InstantCommand(() -> m_intakeSubsystem.setMagazineOff()));
 
       // Intake
-      new JoystickButton(m_gunnerController, Button.kA.value)
-            .toggleWhenActive(new AutomaticIntakeCommand(m_intakeSubsystem));
+      new JoystickButton(m_gunnerController, OIConstants.kIntakeButton)
+         .toggleWhenActive(new AutomaticIntakeCommand(m_intakeSubsystem));
    }
 }
